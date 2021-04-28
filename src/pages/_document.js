8c22cf46403fffd213d159bdcb5f398cb9b67850
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-04-28 13:09:03 -0300

    implementa a funcioalidade de lista

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document{
     render(){
         return(
             <Html>
-                <title>To-doZão - o Seu to-do list preferido!</title>
+                <title>To-doZão | O seu to-do list preferido!</title>
                 <Head>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Freckle+Face&display=swap" rel="stylesheet" /> 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import styles from "../styles/pages/Home.module.css"
 import {Logo} from "../components/Logo.js";
+import {TaskList} from "../components/TaskList.js";
 export default function Home() {
   return(
     <div>
       <div className={styles.container}>
         <Logo />
       <h1>Lista de tarefas</h1>
+      <TaskList />
       </div>
     </div>
   );    
