46922cc4d01f10d658026086e9e78bd9e7a458a7
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-05-10 17:47:11 -0300

    implementa estilos de acessibilidade

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,33 +1,45 @@
-import { useState } from "react"
-import style from "../styles/components/TaskList.module.css"
-export function TaskList(){
-    const [task,setTask]= useState("");
-    const [taskList,setTaskList] = useState([]);
+import { useState } from "react";
+import style from "../styles/components/TaskList.module.css";
 
-    function addTask (event){
-        event.preventDefault();
-        if(task){
-            setTaskList([...taskList,task]);
-            setTask("")
-        }
+export function TaskList() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const[cssCheck,setCssChek]=useState("");
+
+  function addTask(event) {
+    event.preventDefault();
+    if (task) {
+      setTaskList([...taskList, task]);
+      setTask("");
     }
-   
-    function handleInputTask(event){
-        let inputTask =event.target.value
-        setTask(inputTask);
-    }
+  };
 
+  function handleInputTask(event) {
+    let inputTask = event.target.value;
+    setTask(inputTask);
+  };
 
-    return(
-        <div className={style.taskList}>
-            <form>
-                <input type="text" placeholder="Insira sua tarefa aqui" onChange= { handleInputTask} value={task} />
-                <button type="submit" onClick={addTask}>Adcionar</button>
-            </form>
-            <ul>
-                {taskList.map(task=>(<li>{task}</li>))}
-            </ul>
-            
-        </div>
-    )
+  return (
+    <div className={style.taskList}>
+      <form>
+        <input
+          type="text"
+          placeholder="Insira sua tarefa aqui"
+          onChange={handleInputTask}
+          value={task}
+        />
+        <button type="submit" onClick={addTask} className={style.taskButton}>
+          Adcionar
+        </button>
+      </form>
+      <ul>
+        {taskList.map((task) => (
+          <li>
+            {task}
+            <input type="checkbox" className={style.checkTask} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
